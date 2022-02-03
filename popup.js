a4aa05086c25ca56b22258b22661d0d5b26347a2
Akash Chouhan <akash.c1500@gmail.com>
2022-02-03 12:57:11 +0530

    connected to backend API

--- popup.js
+++ popup.js
@@ -2,8 +2,16 @@
 
 let loader = document.getElementById("preloader");
 let loadergif = document.getElementById("loadgif");
+let url = ''
+let ui_elements = {
+  problem_id: "",
+  problem_statement: "",
+  topic: "",
+  link: "",
+};
 
-window.addEventListener("load", function () {
+
+window.addEventListener("load", async function () {
   setTimeout(function () {
     loadergif.classList.add("fade-out-fast");
     loader.classList.add("fade-out");
@@ -15,7 +23,24 @@ window.addEventListener("load", function () {
   let day = date.getDate();
   let datestring = String(day) + String(month + 1);
 
-  console.log(datestring);
+  // Connection to Backend:
+  try{
+    const temp = await fetch(url);
+    const data = await temp.json();
+    console.log(data.response);
+    ui_elements.problem_id = data.response.problem_id;
+    ui_elements.topic = data.response.topic;
+    ui_elements.link = data.response.link;
+    ui_elements.problem_statement = data.response.problem;
+    console.log(ui_elements);
+    console.log(ui_elements.problem_statement);
+
+    // Update UI:
+    updateUI();
+
+}catch(e){ 
+    console.log(e.message);
+  }
 });
 
 //Date Display
@@ -28,11 +53,9 @@ const options = {
 };
 
 dt = dt.toLocaleString("en-US", options);
-
 document.getElementById("date").innerHTML = dt;
 
 //Time Display
-
 function startTime() {
   const date = new Date();
 
@@ -48,26 +71,21 @@ function startTime() {
   setTimeout(startTime, 1000);
 }
 
-// Question description
+const updateUI = ()=>{
+  // Question description
+  let question = ui_elements.problem_statement;
+  if (question.length > 50) {
+    question = question.substring(0, 50) + "... ";
+  }
 
-let question = "Smallest sum contiguous subarray.";
+  document.getElementById("question").innerHTML = question;
+  // Question topic
+  let topic = ui_elements.topic;
+  document.getElementById("topic").innerHTML = topic;
 
-if (question.length > 50) {
-  question = question.substring(0, 50) + "... ";
-}
-
-document.getElementById("question").innerHTML = question;
-
-// Question topic
-
-let topic = "Dynamic Programming";
-
-document.getElementById("topic").innerHTML = topic;
-
-// Question link
-
-let link = "https://www.geeksforgeeks.org/smallest-sum-contiguous-subarray/";
-
-document.getElementById("solve-btn").addEventListener("click", function () {
+  // Question link
+  let link = ui_elements.link;
+  document.getElementById("solve-btn").addEventListener("click", function () {
   window.open(link, "_blank");
-});
+  });
+}
